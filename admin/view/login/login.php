3e2d3946d4b85f290393893e2b4310a39a82fa6a
binhnguyen <nguyenvanbinh3479@gmail.com>
2018-10-19 10:52:56 +0700

    create login page

---
+++ admin/view/login/login.php
@@ -0,0 +1,123 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags-->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="Kevin Anthony">
+    <meta name="author" content="Kevin Anthony">
+    <meta name="keywords" content="Kevin Anthony">
+
+    <!-- Title Page-->
+    <title>Dashboard</title>
+
+    <!-- Fontfaces CSS-->
+    <link href="public/css/font-face.css" rel="stylesheet" media="all">
+    <link href="public/css/font-awesome.min.css" rel="stylesheet" media="all">
+    <link href="public/css/fontawesome-all.min.css" rel="stylesheet" media="all">
+    <link href="public/css/material-design-iconic-font.min.css" rel="stylesheet" media="all">
+
+    <!-- Bootstrap CSS-->
+    <link href="public/css/bootstrap.min.css" rel="stylesheet" media="all">
+
+    <!-- Vendor CSS-->
+    <link href="public/css/animsition.min.css" rel="stylesheet" media="all">
+    <link href="public/css/bootstrap-progressbar-3.3.4.min.css" rel="stylesheet" media="all">
+    <link href="public/css/animate.css" rel="stylesheet" media="all">
+    <link href="public/css/hamburgers.min.css" rel="stylesheet" media="all">
+    <link href="public/css/slick.css" rel="stylesheet" media="all">
+    <link href="public/css/select2.min.css" rel="stylesheet" media="all">
+    <link href="public/css/perfect-scrollbar.css" rel="stylesheet" media="all">
+    <link href="public/css/jqvmap.min.css" rel="stylesheet" media="all">
+
+    <!-- Main CSS-->
+    <link href="public/css/theme.css" rel="stylesheet" media="all">
+
+</head>
+
+<body class="animsition">
+    <div class="page-wrapper">
+        <div class="page-content--bge5">
+            <div class="container">
+                <div class="login-wrap">
+                    <div class="login-content">
+                        <div class="login-logo">
+                            <a href="#">
+                                <img src="public/img/logo.png" alt="Admin">
+                            </a>
+                        </div>
+                        <div class="login-form">
+                            <form action="" method="post">
+                                <input type="hidden" name="c" value="login">
+                                <input type="hidden" name="a" value="index">
+                                <div class="form-group">
+                                    <label>Email Address</label>
+                                    <input class="au-input au-input--full" type="email" name="email" placeholder="Email" required> 
+                                </div>
+                                <div class="form-group">
+                                    <label>Password</label>
+                                    <input class="au-input au-input--full" type="password" name="password" placeholder="Password" required>
+                                </div>
+                                <div class="login-checkbox">
+                                    <label>
+                                        <input type="checkbox" name="remember">Remember Me
+                                    </label>
+                                    <label>
+                                        <a href="#">Forgotten Password?</a>
+                                    </label>
+                                </div>
+                                <button class="au-btn au-btn--block au-btn--green m-b-20" type="submit">sign in</button>
+                                <div class="social-login-content">
+                                    <div class="social-button">
+                                        <button class="au-btn au-btn--block au-btn--blue m-b-20">sign in with facebook</button>
+                                        <button class="au-btn au-btn--block au-btn--blue2">sign in with twitter</button>
+                                    </div>
+                                </div>
+                            </form>
+                            <div class="register-link">
+                                <p>
+                                    Don't you have account?
+                                    <a href="#">Sign Up Here</a>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+    <!-- Jquery JS-->
+    <script src="public/js/jquery-3.3.1.min.js"></script>
+    <!-- Bootstrap JS-->
+    <script src="public/js/popper.min.js"></script>
+    <script src="public/js/bootstrap.min.js"></script>
+    <!-- Vendor JS       -->
+    <script src="public/js/slick.min.js">
+    </script>
+    <script src="public/js/wow.min.js"></script>
+    <script src="public/js/animsition.min.js"></script>
+    <script src="public/js/bootstrap-progressbar.min.js">
+    </script>
+    <script src="public/js/jquery.waypoints.min.js"></script>
+    <script src="public/js/jquery.counterup.min.js">
+    </script>
+    <script src="public/js/circle-progress.min.js"></script>
+    <script src="public/js/perfect-scrollbar.js"></script>
+    <script src="public/js/Chart.bundle.min.js"></script>
+    <script src="public/js/select2.min.js">
+    </script>
+    <script src="public/js/jquery.vmap.js"></script>
+    <script src="public/js/jquery.vmap.min.js"></script>
+    <script src="public/js/jquery.vmap.sampledata.js"></script>
+    <script src="public/js/jquery.vmap.world.js"></script>
+
+    <!-- Main JS-->
+    <script src="public/js/main.js"></script>
+
+</body>
+
+</html>
+<!-- end document-->
