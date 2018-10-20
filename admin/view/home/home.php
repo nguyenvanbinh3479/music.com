f748df2a6460eb784f250731e65fd68599ea8dd3
binhnguyen <nguyenvanbinh3479@gmail.com>
2018-10-20 16:45:20 +0700

    modify template

--- admin/view/home/home.php
+++ admin/view/home/home.php
@@ -4,175 +4,204 @@
 ?>  
 
 
-<div class="row">
-    <div class="col-md-12">
-        <div class="overview-wrap">
-            <h2 class="title-1">overview</h2>
-        </div>
-    </div>
-</div>
-<div class="row m-t-25">
-    <div class="col-sm-12 col-lg-4">
-        <a href="admin.php?c=user">
-            <div class="overview-item overview-item--c1">
-                <div class="overview__inner">
-                    <div class="overview-box clearfix">
-                        <div class="icon">
-                            <i class="zmdi zmdi-account-o"></i>
-                        </div>
-                        <div class="text">
-                            <h2><?php echo count($list_user)?></h2>
-                            <span>Users</span>
-                        </div>
-                    </div>
-                    <div class="overview-chart">
-                        <canvas id="widgetChart1"></canvas>
-                    </div>
-                </div>
-            </div>
-        </a>
-    </div>
-    <div class="col-sm-12 col-lg-4">
-        <a href="admin.php?c=song">
-            <div class="overview-item overview-item--c2">
-                <div class="overview__inner">
-                    <div class="overview-box clearfix">
-                        <div class="icon">
-                            <i class="zmdi zmdi-shopping-cart"></i>
-                        </div>
-                        <div class="text">
-                            <h2>1</h2>
-                            <span>Songs</span>
-                        </div>
-                    </div>
-                    <div class="overview-chart">
-                        <canvas id="widgetChart2"></canvas>
-                    </div>
-                </div>
-            </div>
-        </a>
-    </div>
-    <div class="col-sm-12 col-lg-4">
-        <a href="admin.php?c=singer">
-            <div class="overview-item overview-item--c3">
-                <div class="overview__inner">
-                    <div class="overview-box clearfix">
-                        <div class="icon">
-                            <i class="zmdi zmdi-calendar-note"></i>
-                        </div>
-                        <div class="text">
-                            <h2>1,086</h2>
-                            <span>Singers</span>
-                        </div>
-                    </div>
-                    <div class="overview-chart">
-                        <canvas id="widgetChart3"></canvas>
-                    </div>
-                </div>
-            </div>
-        </a>
-    </div>
-</div>
-
-<!-- STATISTIC-->
-<section class="statistic">
+<!-- BREADCRUMB-->
+<section class="au-breadcrumb m-t-75">
     <div class="section__content section__content--p30">
         <div class="container-fluid">
             <div class="row">
-                <div class="col-md-6 col-lg-3">
-                    <div class="statistic__item">
-                        <a href="admin.php?c=album">
-                            <h2 class="number"><?php echo count($list_user)?> </h2>
-                            <span class="desc">Albums</span>
-                            <div class="icon">
-                                <i class="zmdi zmdi-account-o"></i>
-                            </div>
-                        </a>
-                    </div>
-                </div>
-                <div class="col-md-6 col-lg-3">
-                    <div class="statistic__item">
-                        <a href="admin.php?c=author">
-                            <h2 class="number"><?php echo count($list_user)?> </h2>
-                            <span class="desc">Authors</span>
-                            <div class="icon">
-                                <i class="zmdi zmdi-account-o"></i>
-                            </div>
-                        </a>
-                    </div>
-                </div>
-                <div class="col-md-6 col-lg-3">
-                    <div class="statistic__item">
-                        <a href="admin.php?c=category">
-                            <h2 class="number"><?php echo count($list_user)?> </h2>
-                            <span class="desc">Categories</span>
-                            <div class="icon">
-                                <i class="zmdi zmdi-account-o"></i>
-                            </div>
-                        </a>
-                    </div>
-                </div>
-                <div class="col-md-6 col-lg-3">
-                    <div class="statistic__item">
-                        <a href="admin.php?c=commemt">
-                            <h2 class="number"><?php echo count($list_user)?> </h2>
-                            <span class="desc">Comments</span>
-                            <div class="icon">
-                                <i class="zmdi zmdi-account-o"></i>
-                            </div>
-                        </a>
-                    </div>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-md-6 col-lg-3">
-                    <div class="statistic__item">
-                        <a href="admin.php?c=hear">
-                            <h2 class="number"><?php echo count($list_user)?> </h2>
-                            <span class="desc">Hears</span>
-                            <div class="icon">
-                                <i class="zmdi zmdi-account-o"></i>
-                            </div>
-                        </a>
-                    </div>
-                </div>
-                <div class="col-md-6 col-lg-3">
-                    <div class="statistic__item">
-                        <a href="admin.php?c=like">
-                            <h2 class="number"><?php echo count($list_user)?> </h2>
-                            <span class="desc">Likes</span>
-                            <div class="icon">
-                                <i class="zmdi zmdi-account-o"></i>
-                            </div>
-                        </a>
-                    </div>
-                </div>
-                <div class="col-md-6 col-lg-3">
-                    <div class="statistic__item">
-                        <a href="admin.php?c=playlistdetail">
-                            <h2 class="number"><?php echo count($list_user)?> </h2>
-                            <span class="desc">Playlists Detail</span>
-                            <div class="icon">
-                                <i class="zmdi zmdi-account-o"></i>
-                            </div>
-                        </a>
-                    </div>
-                </div>
-                <div class="col-md-6 col-lg-3">
-                    <div class="statistic__item">
-                        <a href="admin.php?c=playlist">
-                            <h2 class="number"><?php echo count($list_user)?> </h2>
-                            <span class="desc">Playlists</span>
-                            <div class="icon">
-                                <i class="zmdi zmdi-account-o"></i>
-                            </div>
-                        </a>
+                <div class="col-md-12">
+                    <div class="au-breadcrumb-content">
+                        <div class="au-breadcrumb-left">
+                            <span class="au-breadcrumb-span">You are here:</span>
+                            <ul class="list-unstyled list-inline au-breadcrumb__list">
+                                <li class="list-inline-item active">
+                                    <a href="admin.php?c=home">Home</a>
+                                </li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
 </section>
-<!-- END STATISTIC-->
+<!-- END BREADCRUMB-->
+<!-- END PAGE CONTAINER-->
+
+<!-- MAIN CONTENT-->
+<div class="main-content">
+    <div class="section__content section__content--p10">
+        <div class="container-fluid">
+
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="overview-wrap">
+                        <h2 class="title-1">overview</h2>
+                    </div>
+                </div>
+            </div>
+            <div class="row m-t-25">
+                <div class="col-sm-12 col-lg-4">
+                    <a href="admin.php?c=user">
+                        <div class="overview-item overview-item--c1">
+                            <div class="overview__inner">
+                                <div class="overview-box clearfix">
+                                    <div class="icon">
+                                        <i class="zmdi zmdi-account-o"></i>
+                                    </div>
+                                    <div class="text">
+                                        <h2><?php echo count($list_user)?></h2>
+                                        <span>Users</span>
+                                    </div>
+                                </div>
+                                <div class="overview-chart">
+                                    <canvas id="widgetChart1"></canvas>
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-sm-12 col-lg-4">
+                    <a href="admin.php?c=song">
+                        <div class="overview-item overview-item--c2">
+                            <div class="overview__inner">
+                                <div class="overview-box clearfix">
+                                    <div class="icon">
+                                        <i class="zmdi zmdi-shopping-cart"></i>
+                                    </div>
+                                    <div class="text">
+                                        <h2>1</h2>
+                                        <span>Songs</span>
+                                    </div>
+                                </div>
+                                <div class="overview-chart">
+                                    <canvas id="widgetChart2"></canvas>
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-sm-12 col-lg-4">
+                    <a href="admin.php?c=singer">
+                        <div class="overview-item overview-item--c3">
+                            <div class="overview__inner">
+                                <div class="overview-box clearfix">
+                                    <div class="icon">
+                                        <i class="zmdi zmdi-calendar-note"></i>
+                                    </div>
+                                    <div class="text">
+                                        <h2>1,086</h2>
+                                        <span>Singers</span>
+                                    </div>
+                                </div>
+                                <div class="overview-chart">
+                                    <canvas id="widgetChart3"></canvas>
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+            </div>
+
+            <!-- STATISTIC-->
+            <section class="statistic">
+                <div class="section__content section__content--p30">
+                    <div class="container-fluid">
+                        <div class="row">
+                            <div class="col-md-6 col-lg-3">
+                                <div class="statistic__item">
+                                    <a href="admin.php?c=album">
+                                        <h2 class="number"><?php echo count($list_user)?> </h2>
+                                        <span class="desc">Albums</span>
+                                        <div class="icon">
+                                            <i class="zmdi zmdi-account-o"></i>
+                                        </div>
+                                    </a>
+                                </div>
+                            </div>
+                            <div class="col-md-6 col-lg-3">
+                                <div class="statistic__item">
+                                    <a href="admin.php?c=author">
+                                        <h2 class="number"><?php echo count($list_user)?> </h2>
+                                        <span class="desc">Authors</span>
+                                        <div class="icon">
+                                            <i class="zmdi zmdi-account-o"></i>
+                                        </div>
+                                    </a>
+                                </div>
+                            </div>
+                            <div class="col-md-6 col-lg-3">
+                                <div class="statistic__item">
+                                    <a href="admin.php?c=category">
+                                        <h2 class="number"><?php echo count($list_user)?> </h2>
+                                        <span class="desc">Categories</span>
+                                        <div class="icon">
+                                            <i class="zmdi zmdi-account-o"></i>
+                                        </div>
+                                    </a>
+                                </div>
+                            </div>
+                            <div class="col-md-6 col-lg-3">
+                                <div class="statistic__item">
+                                    <a href="admin.php?c=commemt">
+                                        <h2 class="number"><?php echo count($list_user)?> </h2>
+                                        <span class="desc">Comments</span>
+                                        <div class="icon">
+                                            <i class="zmdi zmdi-account-o"></i>
+                                        </div>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-6 col-lg-3">
+                                <div class="statistic__item">
+                                    <a href="admin.php?c=hear">
+                                        <h2 class="number"><?php echo count($list_user)?> </h2>
+                                        <span class="desc">Hears</span>
+                                        <div class="icon">
+                                            <i class="zmdi zmdi-account-o"></i>
+                                        </div>
+                                    </a>
+                                </div>
+                            </div>
+                            <div class="col-md-6 col-lg-3">
+                                <div class="statistic__item">
+                                    <a href="admin.php?c=like">
+                                        <h2 class="number"><?php echo count($list_user)?> </h2>
+                                        <span class="desc">Likes</span>
+                                        <div class="icon">
+                                            <i class="zmdi zmdi-account-o"></i>
+                                        </div>
+                                    </a>
+                                </div>
+                            </div>
+                            <div class="col-md-6 col-lg-3">
+                                <div class="statistic__item">
+                                    <a href="admin.php?c=playlistdetail">
+                                        <h2 class="number"><?php echo count($list_user)?> </h2>
+                                        <span class="desc">Playlists Detail</span>
+                                        <div class="icon">
+                                            <i class="zmdi zmdi-account-o"></i>
+                                        </div>
+                                    </a>
+                                </div>
+                            </div>
+                            <div class="col-md-6 col-lg-3">
+                                <div class="statistic__item">
+                                    <a href="admin.php?c=playlist">
+                                        <h2 class="number"><?php echo count($list_user)?> </h2>
+                                        <span class="desc">Playlists</span>
+                                        <div class="icon">
+                                            <i class="zmdi zmdi-account-o"></i>
+                                        </div>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <!-- END STATISTIC-->
 
 <?php require_once(PATH_PUBLIC . '/template/admin/footer.php'); ?>
--- admin/view/users/create.php
+++ admin/view/users/create.php
@@ -1,48 +1,82 @@
 <?php if ( ! defined('PATH_PUBLIC')) die ('Bad requested!');
     require_once(PATH_PUBLIC . '/template/admin/header.php');
 ?>
-<div id="wrapper">
-	<div class="container">
-	    <form method="post" action="admin.php">
-	    <input type="hidden" name="c" value="user">
-	    <input type="hidden" name="a" value="store">
-	    <div class="row">   		
-	    	<h2>Edit user</h2>
-	    </div>
-	    <div class="row">   		
-	    	<label>Email:</label>
-	    </div>
-	    <div class="row">   		
-	    	<input type="text" class="form-control p-2 m-2" name="email">
-	    </div>
-	    <div class="row">   		
-	    	<label>Password:</label>
-	    </div>
-	    <div class="row">   	
-	    	<input type="text" class="form-control p-2 m-2" name="password">
-	    </div>
-	    <div class="row">   		
-	    	<label>Role:</label>
-	    </div>
-	    <div class="row">
-	    	<select class="form-control p-2 m-2" name="role">
-	    		<option value="admin">Admin</option>
-	    		<option value="user">User</option>
-	    	</select>
-	    </div>
-	    <div class="row">   		
-	    	<label>Status:</label>
-	    </div>
-	    <div class="row">
-	    	<select class="form-control p-2 m-2" name="status">
-	    		<option value="visible">Visible</option>
-	    		<option value="disbale">Disable</option>
-	    	</select>
-	    </div>
-	    <div class="row">   
-	    	<button class="btn btn-primary p-2 m-2" type="submit">Apply</button>
-	    </div>
-	    </form>
-	</div>
-</div>
+<!-- BREADCRUMB-->
+<section class="au-breadcrumb m-t-75">
+    <div class="section__content section__content--p30">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="au-breadcrumb-content">
+                        <div class="au-breadcrumb-left">
+                            <span class="au-breadcrumb-span">You are here:</span>
+                            <ul class="list-unstyled list-inline au-breadcrumb__list">
+                                <li class="list-inline-item active">
+                                    <a href="admin.php?c=home">Home</a>
+                                </li>
+                                <li class="list-inline-item seprate">
+                                    <span>/</span>
+                                </li>
+                                <li class="list-inline-item"><a href="admin.php?c=user" style="color: #999;">User</a></li>
+                            </ul>
+                        </div>
+                        <button class="au-btn au-btn-icon au-btn--green">
+                          <a href="admin.php?c=user" style="color: white;">User</a></button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- END BREADCRUMB-->
+<!-- END PAGE CONTAINER-->
+
+<!-- MAIN CONTENT-->
+<div class="main-content">
+    <div class="section__content section__content--p10">
+        <div class="container-fluid">
+			<div id="wrapper">
+				<div class="container">
+					<form method="post" action="admin.php">
+					<input type="hidden" name="c" value="user">
+					<input type="hidden" name="a" value="store">
+					<div class="row">   		
+						<h2>Create user</h2>
+					</div>
+					<div class="row">   		
+						<label>Email:</label>
+					</div>
+					<div class="row">   		
+						<input type="text" class="form-control p-2 m-2" name="email">
+					</div>
+					<div class="row">   		
+						<label>Password:</label>
+					</div>
+					<div class="row">   	
+						<input type="text" class="form-control p-2 m-2" name="password">
+					</div>
+					<div class="row">   		
+						<label>Role:</label>
+					</div>
+					<div class="row">
+						<select class="form-control p-2 m-2" name="role">
+							<option value="admin">Admin</option>
+							<option value="user">User</option>
+						</select>
+					</div>
+					<div class="row">   		
+						<label>Status:</label>
+					</div>
+					<div class="row">
+						<select class="form-control p-2 m-2" name="status">
+							<option value="visible">Visible</option>
+							<option value="disbale">Disable</option>
+						</select>
+					</div>
+					<div class="row">   
+						<button class="btn btn-primary p-2 m-2" type="submit" onclick="alert('Create success !')">Apply</button>
+					</div>
+					</form>
+				</div>
+			</div>
 <?php require_once(PATH_PUBLIC . '/template/admin/footer.php'); ?>
