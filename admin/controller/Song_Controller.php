614e70f62b8d7d5857cd6f55b0c3663415a1a460
binhnguyen <nguyenvanbinh3479@gmail.com>
2018-10-21 09:33:50 +0700

    modify song

--- admin/controller/Song_Controller.php
+++ admin/controller/Song_Controller.php
@@ -1,69 +1,69 @@
-<?php 
+<?php if ( ! defined('PATH_SYSTEM')) die ('Bad requested!');
 
-  // check system
-  if ( ! defined('PATH_SYSTEM')) die ('Bad requested!');
-
-  class Song_Controller extends Base_Controller{
+class Song_Controller extends Base_Controller
+{
     /**
-    * action index: show all songs
+    * action index: show all baihats
     * method: GET
     */
-      public function index(){        
+    public function index()
+    {        
         $this->model->load('Song');
-        $this->model->load('Singer');
-        $this->model->load('Category');
-        $this->model->load('Author');
-        $this->model->load('Album');
+        // $this->model->load('Singer');
+        // $this->model->load('Album');
+        // $this->model->load('Category');
+        // $this->model->load('Author');
         $list_song = $this->model->Song->all();
-        $list_singer = $this->model->Singer->all();
-        $list_category = $this->model->Category->all();
-        $list_author = $this->model->Author->all();
-        $list_album = $this->model->Album->all();
+        // $list_singer = $this->model->Singer->all();
+        // $list_album = $this->model->Album->all();
+        // $list_category = $this->model->Category->all();
+        // $list_author= $this->model->Author->all();
+
         $data = array(
-          'title' => 'index',
-          'list_song' => $list_song,
-          'list_singer' => $list_singer,
-          'list_category' => $list_category,
-          'list_author' => $list_author,
-          'list_album' => $list_album
+            'title' => 'index',
+            'list_song' => $list_song,
+            'list_singer' => $list_singer,
+            'list_album' => $list_album,
+            'list_category' => $list_category,
+            'list_author' => $list_author
         );
-
-        // Load view
+       
         $this->view->load('songs/index', $data);
-      }
+    }
 
-      /**
-      * action show: show a song
-      * method: GET
-      */
-      public function show(){        
-        $this->model->load('Songs');
-        $song = $this->model->Song->findById($_GET['id']);
+    /**
+    * action show: show a baihat
+    * method: GET
+    */
+    public function show()
+    {        
+        $this->model->load('Song');
+        $song = $this->model->BaiHat->findById($_GET['id']);
         $data = array(
-          'title' => 'show',
-          'song' => $song
+            'title' => 'show',
+            'song' => $song
         );
 
         // Load view
         $this->view->load('songs/show', $data);
-      }
+    }
 
-      /**
-      * action create: create a song
-      * method: GET
-      */
-      public function create(){        
+    /**
+    * action create: create a baihat
+    * method: GET
+    */
+    public function create()
+    {        
 
-        $this->model->load("Album");
-        $this->model->load("Singer");
-        $this->model->load("Category");
-        $this->model->load("Author");
-
+        $this->model->load('Album');
+        $this->model->load('Singer');
+        $this->model->load('Category');
+        $this->model->load('Author');
 
         $list_album = $this->model->Album->all();
         $list_singer = $this->model->Singer->all();
         $list_category = $this->model->Category->all();
-        $list_author = $this->model->Author->all();
+        $list_author= $this->model->Author->all();
 
         $data = array(
             'title' => 'index',
@@ -71,94 +71,96 @@
             'list_singer' => $list_singer,
             'list_category' => $list_category,
             'list_author' => $list_author
-        )
+        );
 
-        $this->view->load('songs/create', $data);
-      }
+        $this->view->load('songs/create',$data);
 
-      /**
-      * action store: save a song to database
-      * method: POST
-      */
-      public function store(){        
+    }
+
+     /**
+    * action store: save a baihat to database
+    * method: POST
+    */
+    public function store()
+    {        
         $this->model->load('Song');
-        $this->model->Song->singer_id = $_POST['singer_id'];
-        $this->model->Song->category_id = $_POST['category_id'];
-        $this->model->Song->author_id = $_POST['author_id'];
-        $this->model->Song->album_id = $_POST['album_id'];
-        $this->model->Song->ten = $_POST['ten'];
-        $this->model->Song->anh = $_POST['anh'];
-        $this->model->Song->loi = $_POST['loi'];
-        $this->model->Song->link = $_POST['link'];
-        $this->model->Song->ngay = $_POST['ngay'];
-        $this->model->Song->save();
+        // $this->model->BaiHat->singers_id = $_POST['singers_id'];
+        // $this->model->BaiHat->album_id = $_POST['album_id'];
+        // $this->model->BaiHat->categories_id = $_POST['categories_id'];
+        // $this->model->BaiHat->author_id = $_POST['author_id'];
+        $this->model->BaiHat->ten = $_POST['ten'];
+        $this->model->BaiHat->anh = $_POST['anh'];
+        $this->model->BaiHat->loi = $_POST['loi'];
+        $this->model->BaiHat->link = $_POST['link'];
+        $this->model->BaiHat->ngay = $_POST['ngay'];
+        $this->model->BaiHat->save();
 
         go_back();
-      }
+    }
 
-      /**
-      * action edit: show form edit a song
-      * method: GET
-      */
-      public function edit(){        
+    /**
+    * action edit: show form edit a baihat
+    * method: GET
+    */
+    public function edit()
+    {        
         $this->model->load('Song');
         $this->model->load('Singer');
         $this->model->load('Album');
         $this->model->load('Category');
         $this->model->load('Author');
 
-        $song = $this->model->Song->findById($_GET['id']);
+        $song = $this->model->BaiHat->findById($_GET['id']);
         $list_album = $this->model->Album->all();
-        $list_author = $this->model->Author->all();
-        $list_category = $this->model->Category->all();
         $list_singer = $this->model->Singer->all();
-
+        $list_category = $this->model->Category->all();
+        $list_author= $this->model->Author->all();
         $data = array(
-          'title' => 'edit',
-          'song' => $song,
-          'list_singer' => $list_singer,
-          'list_album' => $list_album,
-          'list_author' => $list_author,
-          'list_category' => $list_category
+            'title' => 'edit',
+            'song' => $song,
+            'list_singer' => $list_singer,
+            'list_album' => $list_album,
+            'list_category' => $list_category,
+            'list_author' => $list_author
         );
 
         // Load view
         $this->view->load('songs/edit', $data);
-      }
+    }
 
-      /**
-      * action edit: update song database
-      * method: POST
-      */
-      public function update(){        
+    /**
+    * action edit: update baihat database
+    * method: POST
+    */
+    public function update()
+    {        
         $this->model->load('Song');
-        $song = $this->model->Song->findById($_POST['id']);
-        $song->singer_id = $_POST['singer_id'];
+        $song = $this->model->BaiHat->findById($_POST['id']);
+        $song->singers_id = $_POST['singers_id'];
         $song->album_id = $_POST['album_id'];
+        $song->categories_id = $_POST['categories_id'];
         $song->author_id = $_POST['author_id'];
-        $song->category_id = $_POST['category_id'];    
         $song->ten = $_POST['ten'];
         $song->anh = $_POST['anh'];
         $song->loi = $_POST['loi'];
         $song->link = $_POST['link'];
         $song->ngay = $_POST['ngay'];
-        
         $song->update();
 
         go_back();
-      }
+    }
 
-      /**
-      * action delete: show form edit a song
-      * method: GET
-      */
-      public function delete(){        
+    /**
+    * action delete: show form edit a baihat
+    * method: GET
+    */
+    public function delete()
+    {        
         $this->model->load('Song');
-        $song = $this->model->Song->findById($_GET['id']);
+        $song = $this->model->BaiHat->findById($_GET['id']);
         $song->delete();
 
         go_back();
-      }
-  }
+    }
 
-?>
+}
--- admin/view/song/index.php
+++ admin/view/song/index.php
@@ -2,102 +2,175 @@
   if ( ! defined('PATH_PUBLIC')) die ('Bad requested!');
   require_once(PATH_PUBLIC . '/template/admin/header.php');
 ?>  
-<div class="row">
-  <div class="col-md-12">
-    <!-- DATA TABLE -->
-    <h3 class="title-5 m-b-35">data table</h3>
-    <div class="table-data__tool">
-      <div class="table-data__tool-left">
-        <div class="rs-select2--light rs-select2--md">
-            <select class="js-select2" name="property">
-                <option selected="selected">All Properties</option>
-                <option value="">Option 1</option>
-                <option value="">Option 2</option>
-            </select>
-            <div class="dropDownSelect2"></div>
-        </div>
-        <div class="rs-select2--light rs-select2--sm">
-            <select class="js-select2" name="time">
-                <option selected="selected">Today</option>
-                <option value="">3 Days</option>
-                <option value="">1 Week</option>
-            </select>
-            <div class="dropDownSelect2"></div>
-        </div>
-        <button class="au-btn-filter">
-        <i class="zmdi zmdi-filter-list"></i>filters</button>
-      </div>
-      <div class="table-data__tool-right">
-        <button class="au-btn au-btn-icon au-btn--green au-btn--small" type="submit">
-          <a href="admin.php?c=song&a=create" style="color: white;"><i class="zmdi zmdi-plus"></i>add user</a>
-        </button>
-          <div class="rs-select2--dark rs-select2--sm rs-select2--dark2">
-              <select class="js-select2" name="type">
-                  <option selected="selected">Export</option>
-                  <option value="">Option 1</option>
-                  <option value="">Option 2</option>
-              </select>
-              <div class="dropDownSelect2"></div>
-          </div>
-      </div>
-    </div>
-    <div class="table-responsive table-responsive-data2">
-      <table class="table table-data2">
-        <thead>
-            <tr>
-                <th>
-                    <label class="au-checkbox">
-                        <input type="checkbox">
-                        <span class="au-checkmark"></span>
-                    </label>
-                </th>
-                <th>id</th>
-                <th>Singer Id</th>
-                <th>Category Id</th>
-                <th>Author Id</th>
-                <th>Album Id</th>
-                <th>Tên</th>
-                <th>Ảnh</th>
-                <th>Lời</th>
-                <th>Link</th>
-                <th>Ngày</th>
-            </tr>
-        </thead>
-        <tbody>
-          <?php foreach ($list_user as $user) { ?>   
-            <tr class="tr-shadow">
-                <td>
-                    <label class="au-checkbox">
-                        <input type="checkbox">
-                        <span class="au-checkmark"></span>
-                    </label>
-                </td>
-                <td><?php echo $user->id; ?></td>
-                <td><?php echo $user->email; ?></td>
-                <td><?php echo $user->password; ?></td>
-                <td> <span class="status--process"><?php echo $user->role; ?></span></td>
-                <td class="desc"><?php echo $user->status; ?></span></td>
-                <td>
-                    <div class="table-data-feature">
-                      <button class="item" data-toggle="tooltip" data-placement="top" title="Edit">
-                        <a href="admin.php?c=user&a=edit&id=<?php echo $user->id; ?>">
-                          <i class="zmdi zmdi-edit"></i>
-                        </a>
-                      </button>
-                      <button class="item" data-toggle="tooltip" data-placement="top" title="Delete">
-                        <a href="admin.php?c=user&a=delete&id=<?php echo $user->id; ?>">
-                          <i class="zmdi zmdi-delete"></i>
-                        </a>
-                      </button>
+
+<!-- BREADCRUMB-->
+<section class="au-breadcrumb m-t-75">
+    <div class="section__content section__content--p30">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="au-breadcrumb-content">
+                        <div class="au-breadcrumb-left">
+                            <span class="au-breadcrumb-span">You are here:</span>
+                            <ul class="list-unstyled list-inline au-breadcrumb__list">
+                                <li class="list-inline-item active">
+                                    <a href="admin.php?c=home">Home</a>
+                                </li>
+                                <li class="list-inline-item seprate">
+                                    <span>/</span>
+                                </li>
+                                <li class="list-inline-item"><a href="admin.php?c=user" style="color: #999;">User</a></li>
+                            </ul>
+                        </div>
+                        <button class="au-btn au-btn-icon au-btn--green">
+                          <a href="admin.php?c=home" style="color: white;">Home</a></button>
                     </div>
-                </td>
-            </tr>
-            <tr class="spacer"></tr>
-          <?php } ?>
-        </tbody>
-      </table>
+                </div>
+            </div>
+        </div>
     </div>
-    <!-- END DATA TABLE -->
-  </div>
-</div>
+</section>
+<!-- END BREADCRUMB-->
+<!-- END PAGE CONTAINER-->
+
+<!-- MAIN CONTENT-->
+<div class="main-content">
+    <div class="section__content section__content--p10">
+        <div class="container-fluid">
+          <div class="row">
+            <div class="col-md-12">
+              <!-- DATA TABLE -->
+              <h3 class="title-5 m-b-35">data table</h3>
+              <div class="table-data__tool">
+                <div class="table-data__tool-left">
+                  <div class="rs-select2--light rs-select2--md">
+                      <select class="js-select2" name="property">
+                          <option selected="selected">All Properties</option>
+                          <option value="">Option 1</option>
+                          <option value="">Option 2</option>
+                      </select>
+                      <div class="dropDownSelect2"></div>
+                  </div>
+                  <div class="rs-select2--light rs-select2--sm">
+                      <select class="js-select2" name="time">
+                          <option selected="selected">Today</option>
+                          <option value="">3 Days</option>
+                          <option value="">1 Week</option>
+                      </select>
+                      <div class="dropDownSelect2"></div>
+                  </div>
+                  <button class="au-btn-filter">
+                  <i class="zmdi zmdi-filter-list"></i>filters</button>
+                </div>
+                <div class="table-data__tool-right">
+                  <button class="au-btn au-btn-icon au-btn--green au-btn--small" type="submit">
+                    <a href="admin.php?c=song&a=create" style="color: white;"><i class="zmdi zmdi-plus"></i>add Song</a>
+                  </button>
+                    <div class="rs-select2--dark rs-select2--sm rs-select2--dark2">
+                        <select class="js-select2" name="type">
+                            <option selected="selected">Export</option>
+                            <option value="">Excel</option>
+                            <option value="">Word</option>
+                        </select>
+                        <div class="dropDownSelect2"></div>
+                    </div>
+                </div>
+              </div>
+              <div class="table-responsive table-responsive-data2">
+                <table class="table table-data2">
+                  <thead>
+                      <tr>
+                          <th>
+                              <label class="au-checkbox">
+                                  <input type="checkbox">
+                                  <span class="au-checkmark"></span>
+                              </label>
+                          </th>
+                          <th>ID</th>
+                          <th>Singer ID</th>
+                          <th>Category ID</th>
+                          <th>Author ID</th>
+                          <th>Album ID</th>
+                          <th>Ten</th>
+                          <th>Anh</th>
+                          <th>loi</th>
+                          <th>link</th>
+                          <th>Ngay</th>
+                      </tr>
+                  </thead>
+                  <tbody>
+                    <?php foreach ($list_song as $song) { ?>   
+                      <tr class="tr-shadow">
+                          <td>
+                              <label class="au-checkbox">
+                                  <input type="checkbox">
+                                  <span class="au-checkmark"></span>
+                              </label>
+                          </td>
+                          <td><?php echo increment_once($index); ?></td>
+                          <?php 
+                              $arr = (array) $list_singer;
+                              foreach ($arr as $key => $value) {
+                                  $singer = (array) $value;
+                                  if ($singer['id'] == $song->singer_id)   
+                                  echo '<td> '. $singer['ten'] .' </td>';
+                              }
+                          ?>
+
+                          <?php 
+                              $arr = (array) $list_category;
+                              foreach ($arr as $key => $value) {
+                                  $category = (array) $value;
+                                  if ($category['id'] == $song->category_id)   
+                                  echo '<td> '. $category['ten'] .' </td>';
+                              }
+                          ?>
+                          <?php 
+
+                              $arr = (array) $list_author;
+                              foreach ($arr as $key => $value) {
+                                  $author = (array) $value;
+                                  if ($author['id'] == $song->author_id)   
+                                  echo '<td> '. $author['ten'] .' </td>';
+                              }
+                          ?>
+
+                          
+
+                          <?php 
+                              $arr = (array) $list_album;
+                              foreach ($arr as $key => $value) {
+                                  $album = (array) $value;
+                                  if ($album['id'] == $song->album_id)   
+                                  echo '<td> '. $album['ten'] .' </td>';
+                              }
+                          ?>
+                          <td><?php echo $song->ten; ?></td>
+                          <td><img src="public/img/songs/<?php echo $song->anh; ?>" style="width: 50px; height: 50px;"></img></td>
+                          <td><textarea class="form-control" rows="2" cols="30" disabled> <?php echo $song->loi; ?></textarea></td>                                          
+                          <td><?php echo $song->link; ?></td>
+                          <td><?php $date = date_create($song->ngay); echo date_format($date, "d/m/Y"); ?></td>  
+                          <td>
+                              <div class="table-data-feature">
+                                <button class="item" data-toggle="tooltip" data-placement="top" title="Edit">
+                                  <a href="admin.php?c=song&a=edit&id=<?php echo $song->id; ?>">
+                                    <i class="zmdi zmdi-edit"></i>
+                                  </a>
+                                </button>
+                                <button class="item" data-toggle="tooltip" data-placement="top" title="Delete">
+                                  <a href="admin.php?c=song&a=delete&id=<?php echo $song->id; ?>">
+                                    <i class="zmdi zmdi-delete"></i>
+                                  </a>
+                                </button>
+                              </div>
+                          </td>
+                      </tr>
+                      <tr class="spacer"></tr>
+                    <?php } ?>
+                  </tbody>
+                </table>
+              </div>
+              <!-- END DATA TABLE -->
+            </div>
+          </div>
 <?php require_once(PATH_PUBLIC . '/template/admin/footer.php'); ?>
