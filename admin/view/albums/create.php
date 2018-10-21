190303faf25ba62dc241c63c7e2753caaae77f51
binhnguyen <nguyenvanbinh3479@gmail.com>
2018-10-21 16:35:22 +0700

    modify album

---
+++ admin/view/albums/create.php
@@ -0,0 +1,103 @@
+<?php if ( ! defined('PATH_PUBLIC')) die ('Bad requested!');
+    require_once(PATH_PUBLIC . '/template/admin/header.php');
+?>
+<!-- BREADCRUMB-->
+<section class="au-breadcrumb m-t-75">
+    <div class="section__content section__content--p30">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="au-breadcrumb-content">
+                        <div class="au-breadcrumb-left">
+                            <span class="au-breadcrumb-span">You are here:</span>
+                            <ul class="list-unstyled list-inline au-breadcrumb__list">
+                                <li class="list-inline-item active">
+                                    <a href="admin.php?c=home">Home</a>
+                                </li>
+                                <li class="list-inline-item seprate">
+                                    <span>/</span>
+                                </li>
+                                <li class="list-inline-item"><a href="admin.php?c=album" style="color: #999;">album</a></li>                           
+                                <li class="list-inline-item seprate">
+                                    <span>/</span>
+                                </li>
+                                <li class="list-inline-item"><a href="admin.php?c=album&a=create" style="color: #999;">Create album</a></li>                           
+							</ul>
+                        </div>
+                        <button class="au-btn au-btn-icon au-btn--green">
+                          <a href="admin.php?c=album" style="color: white;">album</a></button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- END BREADCRUMB-->
+<!-- END PAGE CONTAINER-->
+
+<!-- MAIN CONTENT-->
+<div class="main-content">
+    <div class="section__content section__content--p10">
+        <div class="container-fluid">
+			<div id="wrapper">
+				<div class="container">
+					<form method="post" action="admin.php">
+					<input type="hidden" name="c" value="album">
+					<input type="hidden" name="a" value="store">
+					<div class="row">   		
+						<h2>Create album</h2>
+					</div>
+                        <div class="row">   		
+                            <label>anh:</label>
+                        </div>
+                        <div class="row">
+                            <label class="btn btn-warning btn-file">
+                                Browse <input type="file" accept="image/*" name="anh" style="display: none;" onchange="loadFile(event)">
+                                <script>
+                                var loadFile = function(event) {
+                                    var output = document.getElementById('output');
+                                    output.src = URL.createObjectURL(event.target.files[0]);
+                                };
+                                </script>
+                            </label>
+                            <img id="output" style="width: 300px; height: 200px;"/>
+						</div>
+                        <div class="row">   		
+                            <label>ten:</label>
+                        </div>
+                        <div class="row">   	
+                            <input type="text" class="form-control p-2 m-2" name="ten" required>
+                        </div>
+                        <div class="row">   		
+                            <label>Ca si:</label>
+                        </div>
+                        <div class="row">
+                            <select class="form-control p-2 m-2" name="Singers_id">
+                                <?php foreach ( $list_singer as $key => $value) { 
+                                    $arr = (array) $value;?>
+                                    <option value="<?php print_r($arr['id']); ?> "> <?php print_r($arr['ten']) ; }?></option>
+                            </select>
+                        </div>
+                        <div class="row">   		
+                            <label>The Loai:</label>
+                        </div>
+                        <div class="row">
+                            <select class="form-control p-2 m-2" name="Categories_id">
+                                <?php foreach ( $list_category as $key => $value) { 
+                                    $arr = (array) $value;?>
+                                    <option value="<?php print_r($arr['id']); ?> "> <?php print_r($arr['ten']) ; }?></option>
+                            </select>
+                        </div>
+						<div class="row">   		
+							<label>ngay:</label>
+						</div>
+						<div class="row">   	
+							<input type="date" class="form-control p-2 m-2" name="ngay" required>
+						</div>
+					<div class="row">   
+						<button class="btn btn-primary p-2 m-2" type="submit">Apply</button>
+					</div>
+					</form>
+				</div>
+			</div>
+<?php require_once(PATH_PUBLIC . '/template/admin/footer.php'); ?>	
---
+++ admin/view/albums/edit.php
@@ -0,0 +1,132 @@
+<?php if ( ! defined('PATH_PUBLIC')) die ('Bad requested!');
+    require_once(PATH_PUBLIC . '/template/admin/header.php');
+?>
+<!-- BREADCRUMB-->
+<section class="au-breadcrumb m-t-75">
+    <div class="section__content section__content--p30">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="au-breadcrumb-content">
+                        <div class="au-breadcrumb-left">
+                            <span class="au-breadcrumb-span">You are here:</span>
+                            <ul class="list-unstyled list-inline au-breadcrumb__list">
+                                <li class="list-inline-item active">
+                                    <a href="admin.php?c=home">Home</a>
+                                </li>
+                                <li class="list-inline-item seprate">
+                                    <span>/</span>
+                                </li>
+                                <li class="list-inline-item"><a href="admin.php?c=album" style="color: #999;">Album</a></li>
+								<li class="list-inline-item seprate">
+                                    <span>/</span>
+                                </li>
+                                <li class="list-inline-item"><a href="admin.php?c=album&a=edit&id=<?php echo $album->id?>" style="color: #999;">Edit Album</a></li>
+                            </ul>
+                        </div>
+                        <button class="au-btn au-btn-icon au-btn--green">
+                          <a href="admin.php?c=album" style="color: white;">Album</a></button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- END BREADCRUMB-->
+<!-- END PAGE CONTAINER-->
+
+<!-- MAIN CONTENT-->
+<div class="main-content">
+    <div class="section__content section__content--p10">
+        <div class="container-fluid">
+			<div id="wrapper">
+				<div class="container">
+					<form method="post" action="admin.php">
+						<input type="hidden" name="id" value="<?php echo $album->id; ?>">
+						<input type="hidden" name="c" value="album">
+						<input type="hidden" name="a" value="update">
+						<div class="row">   		
+							<h2>Edit Album</h2>
+						</div>
+						<div class="row">   		
+							<label>anh:</label>
+						</div>
+						<div class="row">
+                            <label class="btn btn-warning btn-file">
+                                Browse <input type="file" accept="image/*" name="anh" style="display: none;" onchange="loadFile(event)">
+                                <script>
+                                var loadFile = function(event) {
+                                    var output = document.getElementById('output');
+                                    output.src = URL.createObjectURL(event.target.files[0]);
+                                };
+                                </script>
+                            </label>
+                            <img id="output" style="width: 300px; height: 200px;" src="<?php echo $album->anh; ?>"/>
+						</div>
+						<div class="row">   		
+							<label>ten:</label>
+						</div>
+						<div class="row">   	
+							<input type="text" class="form-control p-2 m-2" name="ten" value="<?php echo $album->ten; ?>">
+						</div>
+						<div class="row">   		
+							<label>casi:</label>
+						</div>
+						<div class="row">   	
+							<select class="form-control p-2 m-2" name="Singers_id">
+                                <?php
+                                foreach ($list_singer as $value)
+                                {
+                                    $singer = (array)$value;
+                                    $id = $singer['id'];
+                                    $id_singer = $album->Singers_id;
+                                    $name = $singer['ten'];
+                                    if ($id_singer == $id) {
+                                        echo "<option value='$id' selected>$name
+                                        </option>";
+                                    }else {
+
+                                        echo "<option value='$id'> $name
+                                        </option>";
+                                    }
+                                }            
+                                ?>
+							</select>
+						</div>
+						<div class="row">   		
+							<label>category:</label>
+						</div>
+						<div class="row">   	
+							<select class="form-control p-2 m-2" name="Categories_id">
+                                <?php
+                                foreach ($list_category as $value)
+                                {
+                                    $category = (array)$value;
+                                    $id = $category['id'];
+                                    $id_category = $album->Categories_id;
+                                    $name = $category['ten'];
+                                    if ($id_category == $id) {
+                                        echo "<option value='$id' selected>$name
+                                        </option>";
+                                    }else {
+
+                                        echo "<option value='$id'> $name
+                                        </option>";
+                                    }
+                                }            
+                                ?>
+							</select>
+						</div>
+						<div class="row">   		
+							<label>Ngay:</label>
+						</div>
+						<div class="row">   	
+							<input type="date" class="form-control p-2 m-2" name="ngay" value="<?php echo $album->ngay; ?>">
+						</div>
+						<div class="row">   
+							<button class="btn btn-success p-2 m-2" type="submit">Apply</button>                                             
+						</div>
+					</form>
+				</div>
+			</div>
+<?php require_once(PATH_PUBLIC . '/template/admin/footer.php'); ?>
