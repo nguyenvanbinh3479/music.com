fa139594552d99553adbc3934b28c3d34642270d
binhnguyen <nguyenvanbinh3479@gmail.com>
2018-10-22 16:10:04 +0700

    modify playlist

---
+++ admin/controller/Playlist_Controller.php
@@ -0,0 +1,129 @@
+<?php if ( ! defined('PATH_SYSTEM')) die ('Bad requested!');
+
+class Playlist_Controller extends Base_Controller
+{
+    /**
+    * action index: show all playlists
+    * method: GET
+    */
+    public function index()
+    {        
+        $this->model->load('Playlist');
+        $this->model->load('User');
+
+        $list_playlist = $this->model->Playlist->all();
+        $list_user = $this->model->User->all();
+        
+        $data = array(
+            'title' => 'index',
+            'list_playlist' => $list_playlist,
+            'list_user' => $list_user
+        );
+
+        // Load view
+        $this->view->load('playlists/index', $data);
+    }
+
+    /**
+    * action show: show a playlist
+    * method: GET
+    */
+    public function show()
+    {        
+        $this->model->load('Playlist');
+        $playlist = $this->model->Playlist->findById($_GET['id']);
+        $data = array(
+            'title' => 'show',
+            'playlist' => $playlist
+        );
+
+        // Load view
+        $this->view->load('playlists/show', $data);
+    }
+
+    /**
+    * action create: create a Playlist
+    * method: GET
+    */
+    public function create()
+    {        
+        $this->model->load('User');
+
+        $list_user = $this->model->User->all();
+        
+        $data = array(
+            'title' => 'index',
+            'list_user' => $list_user
+        );
+
+        $this->view->load('playlists/create', $data);
+    }
+
+     /**
+    * action store: save a Playlist to database
+    * method: POST
+    */
+    public function store()
+    {        
+        $this->model->load('Playlist');
+        $this->model->Playlist->Users_id = $_POST['Users_id'];
+        $this->model->Playlist->ten = $_POST['ten'];
+        $this->model->Playlist->anh = $_POST['anh'];
+        $this->model->Playlist->ngay = $_POST['ngay'];
+        $this->model->Playlist->save();
+
+        go_back();
+    }
+
+    /**
+    * action edit: show form edit a Playlist
+    * method: GET
+    */
+    public function edit()
+    {        
+        $this->model->load('Playlist');
+        $this->model->load('User');      
+
+        $playlist = $this->model->Playlist->findById($_GET['id']);
+        $user = $this->model->User->all();
+        
+        $data = array(
+            'title' => 'edit',
+            'playlist' => $playlist,
+            'list_user' => $user
+        );
+
+        // Load view
+        $this->view->load('playlists/edit', $data);
+    }
+
+    /**
+    * action edit: update Playlist database
+    * method: POST
+    */
+    public function update()
+    {        
+        $this->model->load('Playlist');
+        $playlist = $this->model->Playlist->findById($_POST['id']);
+        $playlist->Users_id = $_POST['Users_id'];
+        $playlist->ten = $_POST['ten'];
+        $playlist->anh = $_POST['anh'];      
+        $playlist->ngay = $_POST['ngay'];      
+        $playlist->update();
+
+        go_back();
+    }
+
+    /**
+    * action delete: show form edit a Playlist
+    * method: GET
+    */
+    public function delete()
+    {        
+        $this->model->load('Playlist');
+        $playlist = $this->model->Playlist->findById($_GET['id']);
+        $playlist->delete();
+
+        go_back();
+    }
+}
---
+++ admin/view/playlists/edit.php
@@ -0,0 +1,102 @@
+<?php if ( ! defined('PATH_PUBLIC')) die ('Bad requested!');
+    require_once(PATH_PUBLIC . '/template/admin/header.php');
+?>
+<!-- BREADCRUMB-->
+<section class="au-breadcrumb m-t-75">
+    <div class="section__content section__content--p30">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="au-breadcrumb-content">
+                        <div class="au-breadcrumb-left">
+                            <span class="au-breadcrumb-span">You are here:</span>
+                            <ul class="list-unstyled list-inline au-breadcrumb__list">
+                                <li class="list-inline-item active">
+                                    <a href="admin.php?c=home">Home</a>
+                                </li>
+                                <li class="list-inline-item seprate">
+                                    <span>/</span>
+                                </li>
+                                <li class="list-inline-item"><a href="admin.php?c=playlist" style="color: #999;">Playlist</a></li>
+								<li class="list-inline-item seprate">
+                                    <span>/</span>
+                                </li>
+                                <li class="list-inline-item"><a href="admin.php?c=playlist&a=edit&id=<?php echo $playlist->id?>" style="color: #999;">Edit Playlist</a></li>
+                            </ul>
+                        </div>
+                        <button class="au-btn au-btn-icon au-btn--green">
+                          <a href="admin.php?c=playlist" style="color: white;">Playlist</a></button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- END BREADCRUMB-->
+<!-- END PAGE CONTAINER-->
+
+<!-- MAIN CONTENT-->
+<div class="main-content">
+    <div class="section__content section__content--p10">
+        <div class="container-fluid">
+			<div id="wrapper">
+				<div class="container">
+					<form method="post" action="admin.php">
+						<input type="hidden" name="id" value="<?php echo $playlist->id; ?>">
+						<input type="hidden" name="c" value="playlist">
+						<input type="hidden" name="a" value="update">
+						<div class="row">   		
+							<h2>Edit Playlist</h2>
+						</div>
+						<div class="row">   		
+							<label>User:</label>
+						</div>
+						<div class="row">
+							<select class="form-control p-2 m-2" name="Users_id">
+                            <?php
+                                foreach ($list_user as $value)
+                                {
+                                    $user = (array)$value;
+                                    $id = $user['id'];
+                                    $id_user = $playlist->Users_id;
+                                    $name = $user['email'];
+                                    if ($id_user == $id) {
+                                        echo "<option value='$id' selected>$name
+                                        </option>";
+                                    }else {
+
+                                        echo "<option value='$id'> $name
+                                        </option>";
+                                    }
+                                }            
+                                ?>
+							</select>
+						</div>
+						<div class="row">   		
+							<label>ten:</label>
+						</div>
+						<div class="row">   	
+							<input type="text" class="form-control p-2 m-2" name="ten" value="<?php echo $playlist->ten; ?>">
+						</div>
+						<div class="row">   		
+							<label>anh:</label>
+						</div>
+                        <div class="row">
+                            <label class="btn btn-default btn-file">
+                                Browse <input type="file" accept="image/*" name="anh" style="display: none;" onchange="loadFile(event)">
+                                <script>
+                                var loadFile = function(event) {
+                                    var output = document.getElementById('output');
+                                    output.src = URL.createObjectURL(event.target.files[0]);
+                                };
+                                </script>
+                            </label>
+                            <img id="output" style="width: 300px; height: 200px;" src="public/img/playlists/<?php echo $playlist->anh; ?>"/>
+						</div>
+						<div class="row">   
+							<button class="btn btn-success p-2 m-2" type="submit">Apply</button>                                             
+						</div>
+					</form>
+				</div>
+			</div>
+<?php require_once(PATH_PUBLIC . '/template/admin/footer.php'); ?>
