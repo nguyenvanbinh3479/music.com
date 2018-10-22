bac527de4a1e19a5f8b00d9880deb2f811107a4b
binhnguyen <nguyenvanbinh3479@gmail.com>
2018-10-22 16:31:48 +0700

    create playlist_detail

---
+++ admin/controller/PlaylistDetail_Controller.php
@@ -0,0 +1,152 @@
+<?php if ( ! defined('PATH_SYSTEM')) die ('Bad requested!');
+
+class PlayListDetail_Controller extends Base_Controller
+{
+    /**
+    * action index: show all playlistsdetail
+    * method: GET
+    */
+    public function index()
+    {        
+        $this->model->load('PlayListDetail');
+        $this->model->load('Song');
+        $this->model->load('PlayList');
+        $list_playlistdetail = $this->model->PlayListDetail->all();
+        $list_song = $this->model->Song->all();
+        $list_playlist = $this->model->PlayList->all();
+        $data = array(
+            'title' => 'index',
+            'list_playlistdetail' => $list_playlistdetail,
+            'list_song' => $list_song,
+            'list_playlist' => $list_playlist
+        );
+
+        // Load view
+        $this->view->load('playlistsdetail/index', $data);
+    }
+
+    /**
+    * action show: show a playlistdetail
+    * method: GET
+    */
+    public function show()
+    {        
+        $this->model->load('PlayListDetail');
+        $playlistdetail = $this->model->PlayListDetail->findById($_GET['id']);
+        $data = array(
+            'title' => 'show',
+            'playlistdetail' => $playlistdetail     
+        );
+
+        // Load view
+        $this->view->load('playlistsdetail/show', $data);
+    }
+
+    /**
+    * action create: create a playlistdetail
+    * method: GET
+    */
+    public function create()
+    {        
+        $this->model->load('Song');
+        $this->model->load('PlayList');
+
+        $list_song = $this->model->Song->all();
+        $list_playlist = $this->model->PlayList->all(); 
+
+        $data = array(
+            'title' => 'index',
+            'list_song' => $list_song,
+            'list_playlist' => $list_playlist            
+        );
+        $this->view->load('playlistsdetail/create',$data);
+    }
+
+     /**
+    * action store: save a playlistdetail to database
+    * method: POST
+    */
+    public function store()
+    {        
+        $this->model->load('PlayListDetail');
+        $this->model->PlayListDetail->Playlists_id = $_POST['Playlists_id'];
+        $this->model->PlayListDetail->Songs_id = $_POST['Songs_id'];
+        
+        $this->model->PlayListDetail->save();
+
+        go_back();
+    }
+
+    /**
+    * action edit: show form edit a playlistdetail
+    * method: GET
+    */
+    public function edit()
+    {        
+        $this->model->load('PlayListDetail');
+        $this->model->load('Song');
+        $this->model->load('PlayList');
+
+        $playlistdetail = $this->model->PlayListDetail->findById($_GET['id']);
+        $list_song = $this->model->Song->all();
+        $list_playlist = $this->model->PlayList->all(); 
+
+        $data = array(
+            'title' => 'edit',
+            'playlistdetail' => $playlistdetail,
+            'list_song' => $list_song,
+            'list_playlist' => $list_playlist  
+        );
+
+        // Load view
+        $this->view->load('playlistsdetail/edit', $data);
+    }
+
+    /**
+    * action edit: update playlistdetail database
+    * method: POST
+    */
+    public function update()
+    {        
+
+        $this->model->load('PlayListDetail');
+        $playlistdetail = $this->model->PlayListDetail->findById($_POST['id']);
+        $playlistdetail->Playlists_id = $_POST['Playlists_id'];
+        $playlistdetail->Songs_id = $_POST['Songs_id'];        
+        $playlistdetail->update();
+
+        go_back();
+    }
+
+    public function check_exist() {
+        $conn = FT_Database::instance()->getConnection();
+        $stmt = $conn->prepare("SELECT * FROM playlists WHERE Playlists_id = ? AND Songs_id = ?");
+
+        $stmt->bind_param("ii", $Playlists_id, $Songs_id);
+
+        $stmt->execute();
+        $stmt->bind_result($id);
+        $stmt->store_result();
+        /*Fetch the value*/
+        $stmt->fetch();
+
+        if ($stmt->num_rows > 0) {
+        return $stmt->num_rows;
+        } else {
+        return 0;
+        }
+    }   
+
+
+    /**
+    * action delete: show form edit a playlistdetail
+    * method: GET
+    */
+    public function delete() {        
+        $this->model->load('PlayListDetail');
+        // die($_GET['Playlists_id']);
+        $this->model->PlayListDetail->delete($_GET['Playlists_id'], $_GET['Songs_id']);
+
+        go_back();
+    }
+}
---
+++ model/PlaylistDetail_Model.php
@@ -0,0 +1,50 @@
+<?php 
+class PlayListDetail_Model{
+    public $Playlists_id;
+    public $Songs_id;
+
+    public function all(){
+		$conn = FT_Database::instance()->getConnection();
+		$sql = 'select * from playlistsdetail';
+		$result = mysqli_query($conn, $sql);
+		$list_playlistdetail = array();
+
+		if(!$result)
+			die('Error: '.mysqli_query_error());
+
+		while ($row = mysqli_fetch_assoc($result)){
+            $playlistdetail = new PlayListDetail_Model();
+            $playlistdetail->Playlists_id = $row['Playlists_id'];
+            $playlistdetail->Songs_id = $row['Songs_id'];
+            $list_playlistdetail[] = $playlistdetail;          
+        }
+
+        return $list_playlistdetail;
+	}
+
+	public function save(){
+		$conn = FT_Database::instance()->getConnection();
+		$stmt = $conn->prepare("INSERT INTO playlistsdetail (Playlists_id, Songs_id) VALUES (?, ?)");
+		$stmt->bind_param("ii", $this->Playlists_id, $this->Songs_id);
+		$rs = $stmt->execute();
+		$this->id = $stmt->insert_id;		
+		$stmt->close();
+		return $rs;
+	}
+
+	public function delete($Playlists_id, $Songs_id){
+		$conn = FT_Database::instance()->getConnection();
+		$sql = 'delete from playlistsdetail where Playlists_id ='.$Playlists_id . ' AND Songs_id = ' . $Songs_id;
+		$result = mysqli_query($conn, $sql);
+
+		return $result;
+	}
+
+	public function update(){
+		$conn = FT_Database::instance()->getConnection();
+		$stmt = $conn->prepare("UPDATE playlistsdetail SET Playlists_id=?, Songs_id=? WHERE id=?");
+		$stmt->bind_param("iii", $this->Playlists_id, $this->Songs_id, $_POST['id']);
+		$stmt->execute();
+		$stmt->close();
+	}
+}
