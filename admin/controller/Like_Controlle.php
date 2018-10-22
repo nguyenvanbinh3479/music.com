c98bfbc10859f3382c538d0e613a2cdbb55cf515
binhnguyen <nguyenvanbinh3479@gmail.com>
2018-10-22 15:24:51 +0700

    create like

---
+++ admin/controller/Like_Controlle.php
@@ -0,0 +1,122 @@
+<?php if ( ! defined('PATH_SYSTEM')) die ('Bad requested!');
+
+class Like_Controller extends Base_Controller
+{
+    /**
+    * action index: show all likes
+    * method: GET
+    */
+
+    public function index()
+    {        
+        $this->model->load('Like');
+        $this->model->load('Song');
+        $this->model->load('User');
+        $list_like = $this->model->Like->all();
+        $list_song = $this->model->Song->all();
+        $list_user = $this->model->User->all();
+        $data = array(
+            'title' => 'index',
+            'list_like' => $list_like,
+            'list_song' => $list_song,
+            'list_user' => $list_user
+        );
+
+        // Load view
+        $this->view->load('likes/index', $data);
+    }
+
+
+    /**
+    * action create: create a like
+    * method: GET
+    */
+    public function create()
+    {        
+        $this->model->load('Song');
+        $this->model->load('User');
+        $this->model->load('Like');
+        $like = new Like_Model();
+        $list_song = $this->model->Song->all();
+        $list_user = $this->model->User->all();
+
+        $data = array(
+            'show' => $like->show,
+            'list_song' => $list_song,
+            'list_user' => $list_user,
+        );
+
+        $this->view->load('likes/create',$data);
+
+    }
+
+     /**
+    * action store: save a like to database
+    * method: POST
+    */
+    public function store()
+    {        
+       
+        $this->model->load('Like');
+        if ($this->model->Like->check_Like_exists($_POST['Songs_id'], $_POST['Users_id']) == 0) {
+            $this->model->Like->Songs_id = $_POST['Songs_id'];
+            $this->model->Like->Users_id = $_POST['Users_id'];
+            $this->model->Like->ngay = $_POST['ngay'];
+            $this->model->Like->save();
+            go_back();
+        }else {
+           
+        }
+
+    }
+
+    /**
+    * action edit: show form edit a like
+    * method: GET
+    */
+    public function edit()
+    {        
+        $this->model->load('Like');
+        $this->model->load('Song');
+        $this->model->load('User');
+        $like = $this->model->Like->find($_GET['Songs_id'], $_GET['Users_id']);
+        $list_song = $this->model->Song->all();
+        $list_user = $this->model->User->all();
+        $data = array(
+            'title' => 'edit',
+            'like' => $like,
+            'list_song' => $list_song,
+            'list_user' => $list_user
+        );
+
+        // Load view
+        $this->view->load('likes/edit', $data);
+    }
+
+    /**
+    * action edit: update like database
+    * method: POST
+    */
+    public function update()
+    {        
+        $this->model->load('Like');
+        $like = $this->model->Like->find($_POST['Songs_id'], $_POST['Users_id']);
+        $like->Songs_id = $_POST['Songs_id'];
+        $like->Users_id = $_POST['Users_id'];          
+        $like->ngay = $_POST['ngay'];          
+        $like->update();
+
+        go_back();
+    }
+
+    /**
+    * action delete: show form edit a like
+    * method: GET
+    */
+    public function delete()
+    {        
+        $this->model->load('Like');
+        $this->model->Like->delete($_GET['Songs_id'], $_GET['Users_id']);
+        go_back();
+    }
+}
---
+++ admin/view/likes/edit.php
@@ -0,0 +1,104 @@
+<?php if ( ! defined('PATH_PUBLIC')) die ('Bad requested!');
+    require_once(PATH_PUBLIC . '/template/admin/header.php');
+?>
+<!-- BREADCRUMB-->
+<section class="au-breadcrumb m-t-75">
+    <div class="section__content section__content--p30">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="au-breadcrumb-content">
+                        <div class="au-breadcrumb-left">
+                            <span class="au-breadcrumb-span">You are here:</span>
+                            <ul class="list-unstyled list-inline au-breadcrumb__list">
+                                <li class="list-inline-item active">
+                                    <a href="admin.php?c=home">Home</a>
+                                </li>
+                                <li class="list-inline-item seprate">
+                                    <span>/</span>
+                                </li>
+                                <li class="list-inline-item"><a href="admin.php?c=user" style="color: #999;">User</a></li>
+								<li class="list-inline-item seprate">
+                                    <span>/</span>
+                                </li>
+                                <li class="list-inline-item"><a href="admin.php?c=user&a=edit&id=<?php echo $user->id?>" style="color: #999;">Edit User</a></li>
+                            </ul>
+                        </div>
+                        <button class="au-btn au-btn-icon au-btn--green">
+                          <a href="admin.php?c=user" style="color: white;">User</a></button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- END BREADCRUMB-->
+<!-- END PAGE CONTAINER-->
+
+<!-- MAIN CONTENT-->
+<div class="main-content">
+    <div class="section__content section__content--p10">
+        <div class="container-fluid">
+			<div id="wrapper">
+				<div class="container">
+					<form method="post" action="admin.php">
+						<input type="hidden" name="id" value="<?php echo $user->id; ?>">
+						<input type="hidden" name="c" value="user">
+						<input type="hidden" name="a" value="update">
+						<div class="row">   		
+							<h2>Edit user</h2>
+						</div>
+						<div class="row">   		
+							<label>Email:</label>
+						</div>
+						<div class="row">   		
+							<input type="text" class="form-control p-2 m-2" name="email" value="<?php echo $user->email; ?>" required>
+						</div>
+						<div class="row">   		
+							<label>Password:</label>
+						</div>
+						<div class="row">   	
+							<input type="text" class="form-control p-2 m-2" name="password" value="<?php echo $user->password; ?>" required>
+						</div>
+						<div class="row">   		
+							<label>Role:</label>
+						</div>
+						<div class="row">
+							<select class="form-control p-2 m-2" name="role">
+								<?php 
+									$role = $user->role;
+									if ($role == "admin") {
+										echo "<option value='$role' selected>admin</option>";
+										echo "<option value='user'>user</option>";
+									}else {
+										echo "<option value='$role' selected>user</option>";
+										echo "<option value='admin'>admin</option>";
+									}
+								?>
+							</select>
+						</div>
+						<div class="row">   		
+							<label>Status:</label>
+						</div>
+						<div class="row">
+							<select class="form-control p-2 m-2" name="status">
+								<?php 
+									$status = $user->status;
+									$visible = "visible";
+									if ($status == "visible") {
+										echo "<option value='$status' selected>visible</option>";
+										echo "<option value='disable'>disable</option>";
+									}else {
+										echo "<option value='$status' selected>disable</option>";
+										echo "<option value='$visible'>visible</option>";
+									}
+								?>
+							</select>
+						</div>
+						<div class="row">   
+							<button class="btn btn-success p-2 m-2" type="submit">Apply</button>                                             
+						</div>
+					</form>
+				</div>
+			</div>
+<?php require_once(PATH_PUBLIC . '/template/admin/footer.php'); ?>
