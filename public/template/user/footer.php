53949a865c1cc5c98fb108a81a41058aefb9ebe1
binhnguyen <nguyenvanbinh3479@gmail.com>
2018-10-24 20:21:45 +0700

    modify template

---
+++ public/template/user/footer.php
@@ -0,0 +1,47 @@
+
+                    
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="copyright">
+                                <p>Copyright &copy; <script>document.write(new Date().getFullYear())</script> <a href="https://github.com/nguyenvanbinh3479">Kevin Anthony</a>. All rights reserved.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+    <!-- Jquery JS-->
+    <script src="public/js/jquery-3.3.1.min.js"></script>
+    <!-- Bootstrap JS-->
+    <script src="public/js/popper.min.js"></script>
+    <script src="public/js/bootstrap.min.js"></script>
+    <!-- Vendor JS       -->
+    <script src="public/js/slick.min.js">
+    </script>
+    <script src="public/js/wow.min.js"></script>
+    <script src="public/js/animsition.min.js"></script>
+    <script src="public/js/bootstrap-progressbar.min.js">
+    </script>
+    <script src="public/js/jquery.waypoints.min.js"></script>
+    <script src="public/js/jquery.counterup.min.js">
+    </script>
+    <script src="public/js/circle-progress.min.js"></script>
+    <script src="public/js/perfect-scrollbar.js"></script>
+    <script src="public/js/Chart.bundle.min.js"></script>
+    <script src="public/js/select2.min.js">
+    </script>
+    <script src="public/js/jquery.vmap.js"></script>
+    <script src="public/js/jquery.vmap.min.js"></script>
+    <script src="public/js/jquery.vmap.sampledata.js"></script>
+    <script src="public/js/jquery.vmap.world.js"></script>
+
+    <!-- Main JS-->
+    <script src="public/js/main.js"></script>
+
+</body>
+
+</html>
+<!-- end document-->
